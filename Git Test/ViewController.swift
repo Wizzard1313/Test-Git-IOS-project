9390bcebc8f51c5ce0568bea432e08b673463bd8
Bruce Milyko <Wizzard@Bruces-iMac.home>
2014-08-07 20:44:48 -0400

    Initial Commit

---
+++ Git Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Test
+//
+//  Created by Bruce Milyko on 8/7/14.
+//  Copyright (c) 2014 Wizzard Works. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
