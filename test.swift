e5a71950a9821fce61acc72cb5b371438f31ded8
Wizzard1313 <Wizzard@Bruces-iMac.home>
2014-08-08 13:28:56 -0400

    added a test class

---
+++ test.swift
@@ -0,0 +1,13 @@
+//
+//  test.swift
+//  Git Test
+//
+//  Created by Bruce Milyko on 8/8/14.
+//  Copyright (c) 2014 Wizzard Works. All rights reserved.
+//
+
+import UIKit
+
+class test: NSObject {
+   
+}
